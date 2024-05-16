e25923fb3830c1e7e614629ee0e21b13df6436ea
Dmytro Maniuta <ip722428@gmail.com>
2024-05-16 20:30:50 +0300

    started hw-2

--- src/components/fetchPhotosWithQuery.ts
+++ src/components/fetchPhotosWithQuery.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
-const fetchPhotosWithLoadMore = async (query, page) => {
+const fetchPhotosWithLoadMore = async <T>(
+  query: string,
+  page: number
+): Promise<T> => {
   const params = new URLSearchParams({
-    page: page,
-    per_page: 12,
+    page: page.toString(),
+    per_page: '12',
     orientation: 'landscape',
   });
-
   const CLIENT_ID = 'vTU9R-MFhNM4fikKYX1a6ZvUIFY4QHxnr17U16Q1t-M';
   const URL = `https://api.unsplash.com/search/photos?client_id=${CLIENT_ID}&query=${query}&${params}`;
 
   const response = await axios.get(URL);
   return response.data;
 };
+
 export { fetchPhotosWithLoadMore };
